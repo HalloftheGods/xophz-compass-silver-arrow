fbd915dc1eca39db97810036d7aa18ee97b97385
Xopher <xopher@vi.co>
2026-04-14 21:30:38 -0700

    chore: sync plugins

--- xophz-compass-silver-arrow.php
+++ xophz-compass-silver-arrow.php
@@ -19,7 +19,7 @@
  * Description:     
  * Version:           26.4.14.1311
  * Author:            Hall of the Gods, Inc.
- * Author URI:        http://www.midnightnerd.com/
+ * Author URI:        http://www.hallofthegods.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       xophz-compass-silver-arrow
